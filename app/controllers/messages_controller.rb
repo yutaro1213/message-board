b2dd86e3dfbc35193eb1fa8fc4ba8ee630192b7a
yutaro1213 <wodada19@gmail.com>
2015-10-03 07:57:33 +0000

    kadai1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,17 @@
 class MessagesController < ApplicationController
   def index
    # Messageを全て取得する。
-   @messages=Message.all
+   @messages = Message.all
   end
   
   def create
     @message = Message.new(message_params)
     @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    redirect_to root_path, notice: 'メッセージを保存しました'
   end
 
-  private
-  def message_params
-    params.require(:message).permit(:name, :body)
-  end
-  
+    private
+      def message_params
+        params.require(:message).permit(:name, :body)
+      end
   end
