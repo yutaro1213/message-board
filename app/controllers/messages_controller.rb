d1f87459e26bc8392edbaeb97773d1dc92bf1047
yutaro1213 <wodada19@gmail.com>
2015-10-08 02:35:08 +0000

    destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   def create
@@ -21,5 +40,9 @@ class MessagesController < ApplicationController
   
     def message_params
       params.require(:message).permit(:name, :body)
+    end
+    
+    def set_message
+      @message = Message.find(params[:id])
     end
 end
