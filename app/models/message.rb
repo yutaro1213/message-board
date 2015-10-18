835140a78270aafed986f475733edfbdae036af3
yutaro1213 <wodada19@gmail.com>
2015-10-18 11:08:26 +0000

    age3

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,9 @@
 class Message < ActiveRecord::Base
+    # 名前は必須入力かつ20文字以内
+    validates :name , length: {  maximum: 20 } , presence: true
+    # 年齢は必須入力かつ数字のみ,0歳以上99歳以下
+    validates :age , inclusion: { in: 0..99 }, presence: true
+    validates :age , inclusion: { in: 0..99 }, numericality: true, presence: true
+    # 内容は必須入力かつ2文字以上30文字以下
+    validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
 end
